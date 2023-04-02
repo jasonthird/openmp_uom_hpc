bddd55fa36315ec46af8e690ec71765d1cb68fc0
Iason Malkotsis <imalkotsis@gmail.com>
2023-04-02 13:00:09 +0300

    made some changed to jacobi heat

--- jacobi heat/jacobi_heat.c
+++ jacobi heat/jacobi_heat.c
@@ -12,68 +12,63 @@
 
 int main(int argc, char* argv[])
 {
-  int i, j, k;
-  double table1[maxsize][maxsize];
-  double table2[maxsize][maxsize];
-  double diff;
+	int i, j, k;
+	float table1[maxsize][maxsize];
+	float table2[maxsize][maxsize];
+	double diff;
 
-  //set number of threads
-  omp_set_num_threads(num_threads);
+	//set number of threads
+	omp_set_num_threads(num_threads);
 
-  /* initialize both tables*/
+	/* initialize both tables*/
 
-  for(i=0;i<maxsize;i++)
-    for(j=0;j<maxsize;j++)
-    {
-      table1[i][j]=0;
-      table2[i][j]=0;
-    }
+	for(i=0;i<maxsize;i++)
+		for(j=0;j<maxsize;j++)
+		{
+			table1[i][j]=0;
+			table2[i][j]=0;
+		}
 
-  /* repeate for each iteration */
-  for(k = 0; k < iterations; k++) 
-  {
-    
-    /* create a heat source */
-    table1[row][col] = start;
-    
-    /* difference initialization */
-    diff = 0.0;
+  	/* repeate for each iteration */
+	for(k = 0; k < iterations; k++) 
+	{
+	  
+		/* create a heat source */
+		table1[row][col] = start;
+		
+		/* difference initialization */
+		diff = 0.0;
 
-  #pragma omp parallel for private(i,j) shared (table1,table2) reduction(+:diff) schedule(auto)
-  for(i=1;i<maxsize-1;i++){
-      double temp[maxsize-2][maxsize-2];
-      for(j=1;j<maxsize-1;j++) {
-          temp[i-1][j-1] = 0.25 *(table1[i-1][j] + table1[i+1][j] + table1[i][j-1] + table1[i][j+1]);
-          diff += (temp[i-1][j-1]-table1[i][j])*(temp[i-1][j-1]-table1[i][j]);
-      }
-      for(j=1;j<maxsize-1;j++) {
-          table2[i][j] = temp[i-1][j-1];
-      }
-  }
+		#pragma omp parallel for private(i,j) shared (table2) default (shared) reduction(+:diff)
+			for(i=1;i<maxsize-1;i++)
+				for(j=1;j<maxsize-1;j++) {
+					table2[i][j] = 0.25 *(table1[i-1][j] + table1[i+1][j] + table1[i][j-1] + table1[i][j+1]);
+					diff += (table2[i][j]-table1[i][j])*(table2[i][j]-table1[i][j]);
+				}
 
-    // /* print result */
-    // for(i=0;i<maxsize;i++)
-    // {
-    //   for(j=0;j<maxsize;j++)
-    //     printf("%5.0f ",table2[i][j]);
-    //   printf("\n");
-    // }
-    // printf("\n");
-  
-    /* print difference and check convergence */
-    diff = sqrt(diff);
-    //printf("diff = %3.25f\n\n", diff);
+		/* print result */
+		// for(i=0;i<maxsize;i++)
+		// {
+		// 	for(j=0;j<maxsize;j++)
+		// 	printf("%5.0f ",table2[i][j]);
+		// 	printf("\n");
+		// }
+		// printf("\n");
+		
+		/* print difference and check convergence */
+		diff = sqrt(diff);
+		//printf("diff = %3.25f\n\n", diff);
 
-    if (diff < accuracy) {
-    printf ("\n\nConvergence in %d iterations\n\n", k);
-          break;
-  }	
-  
-  /* copy new table to old table */ 
-    for(i=0;i<maxsize;i++)
-      for(j=0;j<maxsize;j++)
-        table1[i][j]=table2[i][j];
-  }
+		if (diff < accuracy) {
+		printf ("\n\nConvergence in %d iterations\n\n", k);
+				break;
+		}	
+		
+		/* copy new table to old table */ 
+		for(i=0;i<maxsize;i++)
+			for(j=0;j<maxsize;j++)
+			table1[i][j]=table2[i][j];
+	}
 
-  return 0;
+	return 0;
 }
---
+++ jacobi heat/jacobi_heat_ublassMatrix_attempt_SLOW.cpp
@@ -0,0 +1,41 @@
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <cmath>
+#include "omp.h"
+
+#define MAXSIZE 500
+#define ITERATIONS 1000000
+#define ROW 50
+#define COL 50
+#define START 100.0
+#define ACCURACY 27
+#define NUM_THREADS 16
+
+int main(){
+    int i, j, k;
+    double diff = 0;
+    boost::numeric::ublas::matrix<float> table1 = boost::numeric::ublas::zero_matrix<double>(MAXSIZE, MAXSIZE);
+    boost::numeric::ublas::matrix<float> table2 = boost::numeric::ublas::zero_matrix<double>(MAXSIZE, MAXSIZE);
+    omp_set_num_threads(NUM_THREADS);
+    // iteractions
+    for(k = 0; k < ITERATIONS; k++){
+        table1 (ROW, COL) = START;
+        diff = 0.0;
+        #pragma omp parallel for private(i,j) shared(table2) reduction(+:diff)
+        for(i=1;i<MAXSIZE-1;i++)
+            for(j=1;j<MAXSIZE-1;j++) {
+            float y = 0.25 *(table1(i-1,j) + table1(i+1,j) + table1(i,j-1) + table1(i,j+1));
+            table2.insert_element(i,j,y);
+            diff += (table2(i,j)-table1(i,j))*(table2(i,j)-table1(i,j));
+            }
+    
+        diff = sqrt(diff);
+        //printf("diff = %3.25f\n\n", diff);        
+        if (diff < ACCURACY) {
+            printf ("\n\nConvergence in %d iterations\n\n", k);
+            break;
+        }
+        std::swap(table1, table2);
+    }
+}
+
